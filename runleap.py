fdb93dba88d7d6a633449e3d349a6e4d4fbb87e2
AMantes <ach.mantes@gmail.com>
2023-08-07 11:49:24 +0300

    Update runleap.py
    Refactor script into functional programming and add LEAP export functionality
    
    - Organized the script into functions for improved maintainability
    - Added the ability to export LEAP data to CSV files for analysis
    - Updated logging to log results and errors for better debugging

--- runleap.py
+++ runleap.py
@@ -1,25 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-The script opens LEAP, loads version It-leds maroc v 0.101_wresults.
-It loops through the scenarios, computes, then saves as CSV social costs per sector and closes the app.
-At the momemt units and costs saved as favourite tables are preset in leap.
+Created on Mon Aug  7 11:43:49 2023
 
-
-@author: Achilleas Mantes
+@author: Achilleas
 """
 
-import win32com.client as win32
-import pandas as pd
-import subprocess
-import logging
-import csv
 import os
-
+import logging
+import win32com.client as win32
 
 # Set the working directory
 working_directory = r"C:\Users\Achilleas\Documents\Gemmes"
 os.chdir(working_directory)
-
 
 # Set up logging
 logging.basicConfig(
@@ -28,116 +20,60 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
-
-
-
-# Verify the current working directory
-current_directory = os.getcwd()
-logging.info("Current working directory: %s", current_directory)
-
-
-
-# Set up LEAP
-try:
-    leap = win32.Dispatch('LEAP.LEAPApplication')
-    logging.info("LEAP opens successfully.")
-except Exception as e:
-    logging.error("Error occurred while opening LEAP: %s", str(e))
-
-    
-    leap.Visible = True
-    
-try:    
-    leap.ActiveArea = "lt-leds maroc v0.101_wresults"  # Morocco v101 
-    logging.info("Morocco area opens successfully.")
-except Exception as e:
-    logging.error("Error occurred while setting the active area in LEAP: %s", str(e))
-    
-    
-    
-# List of favorite charts
-favorite_charts = ['industry', 'residential', 'tertiaire', 'transport', 'transformation', 'resources', 'non_energy']
-
-# List of scenarios
-scenarios = ['Référence', 'CDN Plus', 'Net zéro']
-
-# Loop through the scenarios
-for scenario in scenarios:
+def open_leap():
     try:
-        # Set the active scenario
-        leap.ActiveScenario = scenario
-        leap.ActiveView = "Results"
+        leap = win32.Dispatch('LEAP.LEAPApplication')
+        leap.Visible = True
+        leap.ActiveArea = "lt-leds maroc v0.101_wresults"  # Morocco v101 
+        logging.info("LEAP initialized successfully.")
+        return leap
+    except Exception as e:
+        logging.error("Error occurred while initializing LEAP: %s", str(e))
+        return None
 
-        # Loop through the favorite charts and export each one as a CSV
-        for chart in favorite_charts:
-            leap.Favorites(chart).Activate()
-
-            # Set the file name for the CSV export
-            csv_file_name = f"{scenario}_{chart}.csv"
-
-            # Concatenate the working directory and the file name to create the full path
-            csv_file_path = os.path.join(working_directory, csv_file_name)
-
-            # Export the current favorite chart as a CSV
-            leap.ExportresultsCSV(csv_file_path)
-            logging.info(f"Results for scenario '{scenario}', chart '{chart}' successfully exported to '{csv_file_path}'.")
+def export_favorite_chart(leap, chart, scenario, working_directory):
+    try:
+        leap.Favorites(chart).Activate()
+        csv_file_name = f"{scenario}_{chart}.csv"
+        csv_file_path = os.path.join(working_directory, csv_file_name)
+        leap.ExportresultsCSV(csv_file_path)
+        logging.info(f"Results for scenario '{scenario}', chart '{chart}' successfully exported to '{csv_file_path}'.")
     except Exception as e:
         logging.error(f"Error occurred while exporting results for scenario '{scenario}', chart '{chart}': {str(e)}")
+
+def close_leap(leap):
+    try:
+        leap.Quit()
+        logging.info("LEAP closed successfully.")
+    except Exception as e:
+        logging.error("Error occurred while closing LEAP: %s", str(e))
+
+def main():
+    # Open LEAP
+    leap = open_leap()
     
+    if leap:
+        # List of favorite charts
+        favorite_charts = ['industry', 'residential', 'tertiaire', 'transport', 'transformation', 'resources', 'non_energy']
 
-# =============================================================================
+        # List of scenarios
+        scenarios = ['Référence', 'CDN Plus', 'Net zéro']
 
-# To be cleaned later
+        # Loop through the scenarios
+        for scenario in scenarios:
+            try:
+                # Set the active scenario
+                leap.ActiveScenario = scenario
+                leap.ActiveView = "Results"
+
+                # Loop through the favorite charts and export each one as a CSV
+                for chart in favorite_charts:
+                    export_favorite_chart(leap, chart, scenario, working_directory)
+            except Exception as e:
+                logging.error(f"Error occurred while processing scenario '{scenario}': {str(e)}")
         
-   # baseyear = leap.BaseYear
-    #logging.info("LEAP initialized successfully.")
-    # Log leap setup information
-    #logging.debug("LEAP setup: Visible=%s, ActiveArea=%s, BaseYear=%d", leap.Visible, leap.ActiveArea, baseyear)
+        # Close LEAP
+        close_leap(leap)
 
-#except Exception as e:
-#    logging.error("Error occurred while initializing LEAP: %s", str(e))
-
-#print(baseyear) 
-# #### Load Scenarios
-# 
-# # Load the "Baseline" scenario
-# #scenario_name = "Net zéro"  #"Current Accounts"  #"Référence"
-# #scenario = leap.Scenarios(scenario_name).ResultsShown
-# 
-# #if scenario:
-# #    logging.info("Scenario '%s' loaded successfully.", scenario_name)
-# #else:
-# #    logging.error("Scenario '%s' could not be loaded.", scenario_name)
-# 
-# 
-# 
-# #Runs everything
-# # leap.Calculate()
-# leap.ActiveScenario = "Net zéro"
-# leap.ActiveView="Results"
-# 
-# 
-# # Check if the active scenario was set successfully
-# if scenario:
-#     logging.info(f"Successfully set '{scenario_name}' as the active scenario.")
-#     
-#     # Run calculations for the active scenario
-#     try:
-#         scenario.Calculate()
-#         logging.info(f"Scenario '{scenario_name}' calculations completed successfully.")
-#     except Exception as e:
-#         logging.error(f"Error occurred while running scenario '{scenario_name}' calculations: {str(e)}")
-# else:
-#     logging.error(f"Failed to set '{scenario_name}' as the active scenario.")
-# =============================================================================
-
-
-
-
-# =============================================================================
-# Close LEAP
-try:
-    leap.Quit()
-    logging.info("LEAP closed successfully.")
-except Exception as e:
-    logging.error("Error occurred while closing LEAP: %s", str(e))
+if __name__ == "__main__":
+    main()
